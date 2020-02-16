85d1bd5a551b3d0bf53db0666c802cb535b611ec
Kristiyan <kristiyan.stoyanov99@icloud.com>
2020-02-16 16:52:53 +0200

    added help method

--- Main.rb
+++ Main.rb
@@ -4,8 +4,25 @@ require_relative 'OrderHandler'
 cakeHandler = CakeHandler.new
 orderHandler = OrderHandler.new
 
+def printHelp
+    puts "The program expects commands in the following form"
+    puts "{commandName}: {param1}, {param2}, ... {paramN}"
+    puts "Here is a list of all the possible commands and how they work:"
+    puts "createCake: {name}, {price}, {weight}, {description}, {timeToComplete (in minutes)} #creates a cake and adds it to the database"
+    puts "beginOrder #begins a new order, returning the id of the order in order to be manipulated in the future"
+    puts "addToOrder: {orderId}, {name of cake}, {quantity} #adds the cake identified by the name to the cart as many times as the quantity param"
+    puts "payOrder: {orderId} #changes the status of the order to paid and adds it to the balance sheet"
+    puts "finishOrder: {orderId} #changes the status of the order to finished"
+    puts "balanceSheet #displays the balance sheet after all paid orders"
+    puts "viewOrder: {orderId} #displays information about the order"
+    puts "help #describes all the commands"
+end
+
+puts "Please enter a command: "
 line = gets.chomp
+
 while (line != "quit")
+    puts "Please enter a command: "
     tokens = line.split(': ')
     command = tokens[0]
     tokens = tokens[1].split(', ') if tokens[1]
@@ -26,7 +43,7 @@ while (line != "quit")
     when "viewOrder"
         orderHandler.viewOrder(tokens[0].to_i)
     when "help"
-        #todo
+        printHelp
     end
 
     line = gets.chomp
